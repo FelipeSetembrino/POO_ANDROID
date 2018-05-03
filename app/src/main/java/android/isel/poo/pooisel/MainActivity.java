8f273d181826e00a5782ab864831a6557a52e36e
FelipeSetembrino <felipe.setembrino@gmail.com>
2018-05-03 21:34:03 +0100

    Botoes
    Adicionado os três botões;
    Criado três layout:
    1 - Global;
    2 - control;
    3 - radio_layout.
    
    Global: content da tela toda;
    Control: content horizontal com os três botoes;
    radio_layout: Content NÃO FINALIZADO. Necessário adicoionar os botões de radio. Professor ainda não explicou.
    
    Co-Authored-By: AntonioDe <desyatnik@litecfd.net>

--- app/src/main/java/android/isel/poo/pooisel/MainActivity.java
+++ app/src/main/java/android/isel/poo/pooisel/MainActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Adição view da tela:
+    private int TX_BUTTON_SIZE = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +18,45 @@ public class MainActivity extends AppCompatActivity {
         // setup view
         LinearLayout global = new LinearLayout(this);
         LinearLayout control = new LinearLayout(this);
+        LinearLayout radio_layout = new LinearLayout(this);
+
+        //Iniciando a orientação das View
+        global.setOrientation(LinearLayout.VERTICAL);
+        control.setOrientation(LinearLayout.HORIZONTAL);
+        radio_layout.setOrientation(LinearLayout.HORIZONTAL);
 
         //Crinado os butões:
         //Botão 1
         Button button1 = new Button(this);
         button1.setText("Button 1");
-        button1.setTextSize(30);
+        button1.setTextSize(TX_BUTTON_SIZE);
         //Botão 2
         Button button2 = new Button(this);
         button2.setText("Button 2");
-        button2.setTextSize(30);
+        button2.setTextSize(TX_BUTTON_SIZE);
         //Botão 3
         Button button3 = new Button(this);
         button3.setText("Button 3");
-        button3.setTextSize(30);
+        button3.setTextSize(TX_BUTTON_SIZE);
+        //Radio Button
+        RadioButton radio_button = new RadioButton(this);
 
-        //Iniciando a orientação das View
-        global.setOrientation(LinearLayout.VERTICAL);
-        control.setOrientation(LinearLayout.HORIZONTAL);
 
-        
+
+        //Adicionando os botões no Control:
+        control.addView(button1);
+        control.addView(button2);
+        control.addView(button3);
+
+        //Adicionando botões radio no radio_layout
+        radio_layout.addView(radio_layout);
+
+        //Adicionando Control no Global:
+        global.addView(control);
+        global.addView(radio_layout);
+
+        //Iniciando tela:
+        setContentView(global);
 
 
     }
